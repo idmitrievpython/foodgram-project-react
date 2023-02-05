0a2a3234055a7826fa6ca8a26c71c65ba13c620b
Ilya Dmitriev <dmitriev_ilya19@mail.ru>
2023-02-05 13:08:03 +0400

    food

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.core import validators
 from django.db import models
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
