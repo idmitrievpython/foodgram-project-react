900564a0533cf4db606f0af63b447e036fe01e98
Ilya Dmitriev <dmitriev_ilya19@mail.ru>
2023-02-12 15:49:01 +0400

    hw14

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -65,7 +65,7 @@ DATABASES = {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='xxxyyyzzz'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='admin'),
         'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default='5432')
     }
